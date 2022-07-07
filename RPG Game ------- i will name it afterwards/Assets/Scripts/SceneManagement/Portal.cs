470757210c8748a4637b50f789d5b41298394c1b
Rishit-cloud <rishitnagar87@gmail.com>
2022-07-08 01:39:08 +0530

    Action RPG

---
+++ RPG Game ------- i will name it afterwards/Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+                A, B, C, D, E
+        }
+        
+        
+        [SerializeField] int buildSceneIndex;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+         
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(buildSceneIndex);
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+                return portal;
+            }
+
+            return null;
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+    }
+}
